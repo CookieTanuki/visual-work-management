89b04450029d3ffff74ad2df3e335ad077ffcc58
Nick <nikitosi3012@gmail.com>
2025-12-22 19:10:37 +0200

    add login/logout redirect urls to settings

--- VisualWorkManagement/settings.py
+++ VisualWorkManagement/settings.py
@@ -91,6 +91,10 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = "core.Worker"
 
 
+LOGOUT_REDIRECT_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
